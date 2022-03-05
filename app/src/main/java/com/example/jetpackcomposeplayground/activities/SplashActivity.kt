1b11d64e458e5f70ad58ec0eefb44ce60700020e
junior-od <opeyemi.oduberu@gmail.com>
2022-03-05 21:55:03 +0100

    text fields and uttons understood

--- app/src/main/java/com/example/jetpackcomposeplayground/activities/SplashActivity.kt
+++ app/src/main/java/com/example/jetpackcomposeplayground/activities/SplashActivity.kt
@@ -7,30 +7,36 @@ import androidx.activity.viewModels
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.TileMode
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.jetpackcomposeplayground.R
 import com.example.jetpackcomposeplayground.ui.theme.JetpackComposePlaygroundTheme
 import com.example.jetpackcomposeplayground.ui.theme.Typography
 import com.example.jetpackcomposeplayground.viewmodels.SplashViewModel
+import kotlinx.coroutines.launch
+import kotlin.random.Random
 
 class SplashActivity : ComponentActivity() {
     private val splashViewModel: SplashViewModel by viewModels()
@@ -43,22 +49,106 @@ class SplashActivity : ComponentActivity() {
         }
 
         setContent {
-            JetpackComposePlaygroundTheme() {
-                Column(modifier = Modifier
-                    .fillMaxWidth()
-                    .fillMaxHeight()) {
-                    ImageCard(img = R.drawable.fine , title = "Robot" , contentDesc = "I am a\nlovely flower.")
-                }
+            val scaffoldState = rememberScaffoldState()
+            var textFieldState by remember {
+                mutableStateOf("")
             }
+            val scope = rememberCoroutineScope() //scope we can use within a composable, used only for callbacks
+            Scaffold(modifier = Modifier.fillMaxSize(),
+                    scaffoldState = scaffoldState) {
+                    Column(horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center,
+                        modifier = Modifier.fillMaxSize()) {
+//                        Greeting2(name = textFieldState)
+                        TextField(value = textFieldState ,
+                            singleLine = true,
+                            textStyle = Typography.h1,
+//                            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                            label = {
+                             Text(text = "Enter your name") } ,
+                            onValueChange = {
+                            textFieldState = it
+                        })
+
+                        Spacer(modifier = Modifier.padding(vertical = 5.dp))
+                        Button(onClick = {
+                            scope.launch {
+                                scaffoldState.snackbarHostState.showSnackbar(message = "Hi $textFieldState",duration = SnackbarDuration.Short)
+                            }
+                        }) {
+                            Text(text = "Greet Me")
+                        }
+
+
+                    }
+//                Snackbar {
+//                    Text(text = "hey there")
+//                }
+            }
+            
+
+            //playground stuff
+//            JetpackComposePlaygroundTheme() {
+//                val color = remember {
+//                    mutableStateOf(Color.Black)
+//                }//state, remember is so that it doesn't call the initial value again after first draw on create
+//
+//                Column(modifier = Modifier
+//                    .fillMaxSize()) {
+//                    ImageCard(img = R.drawable.fine ,
+//                        title = "Robot" ,
+//                        contentDesc = "I am a\nlovely flower.")
+//
+//                    Text(text = buildAnnotatedString {
+//                        append("Don't have an account? ")
+//                        withStyle(
+//                            style = SpanStyle(
+//                                color = color.value,
+//                                fontSize = 14.sp,
+//                                fontWeight = FontWeight.Bold,
+//
+//                            )
+//                        ){
+//                            append("Sign Up ")
+//                        }
+//
+//                    },
+//                        color = Color.Gray,
+//                        style = Typography.body1
+//                    )
+//                    ColorBox(color, modifier = Modifier.fillMaxSize(0.1f)){
+//                        color.value = it
+//                    }
+//                }
+//            }
         }
 
     }
 }
 
 @Composable
+fun ColorBox(color: MutableState<Color>,modifier: Modifier, updateColor: (Color) -> Unit){
+//    val color = remember {
+//        mutableStateOf(Color.Black)
+//    }//state, remember is so that it doesn't call the initial value again after first draw on create
+    Box(modifier = modifier
+        .background(color.value)
+        .clickable {
+            updateColor(
+                Color(
+                    Random.nextFloat(), Random.nextFloat(), Random.nextFloat(), 1f
+                )
+            )
+        }) {
+
+    }
+
+}
+
+@Composable
 fun Greeting2(name: String) {
     Text(text = "Hello $name!", modifier = Modifier
-        .padding(top = 10.dp))
+        .padding(vertical = 10.dp))
 }
 
 @Composable
@@ -73,7 +163,9 @@ fun SplashImage(img: Int){
 fun ImageCard(img : Int, title: String, contentDesc: String, modifier: Modifier = Modifier){
 
     Card (
-        modifier = modifier.fillMaxWidth(0.5f).padding(10.dp),
+        modifier = modifier
+            .fillMaxWidth(0.5f)
+            .padding(10.dp),
         shape = RoundedCornerShape(15.dp),
         elevation = 5.dp
     ) {
@@ -84,15 +176,32 @@ fun ImageCard(img : Int, title: String, contentDesc: String, modifier: Modifier 
                 contentScale = ContentScale.Crop
             )
 
-            Box(modifier = Modifier.fillMaxSize()
-                .background(Brush.verticalGradient(listOf(
-                    Color.Transparent,Color.Black), startY = 300f, endY = Float.POSITIVE_INFINITY, tileMode = TileMode.Clamp )))
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .background(
+                    Brush.verticalGradient(
+                        listOf(
+                            Color.Transparent, Color.Black
+                        ), startY = 300f, endY = Float.POSITIVE_INFINITY, tileMode = TileMode.Clamp
+                    )
+                ))
 
-            Box(modifier = Modifier.padding(10.dp).fillMaxSize(),
+            Box(modifier = Modifier
+                .padding(10.dp)
+                .fillMaxSize(),
                 contentAlignment = Alignment.BottomStart){
                 Column() {
-                    Text(text = title, style = Typography.h1, color = Color.White )
-                    Text(text = contentDesc , style = Typography.body1, color = Color.White)
+                    Text(text = title,
+                        style = Typography.h1,
+                        color = Color.White,
+                        textDecoration = TextDecoration.Underline ,
+                        modifier = Modifier.fillMaxWidth(),
+                        textAlign = TextAlign.Center )
+                    Text(text = contentDesc ,
+                        style = Typography.body1,
+                        color = Color.White,modifier = Modifier.fillMaxWidth(),
+                        textAlign = TextAlign.Center,
+                        textDecoration = TextDecoration.LineThrough)
                 }
             }
 
--- app/src/main/java/com/example/jetpackcomposeplayground/ui/theme/Type.kt
+++ app/src/main/java/com/example/jetpackcomposeplayground/ui/theme/Type.kt
@@ -2,13 +2,26 @@ package com.example.jetpackcomposeplayground.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.jetpackcomposeplayground.R
+
+//declare custom fonts here
+val fontFamily = FontFamily(
+    Font(R.font.oswald_bold, FontWeight.Bold),
+    Font(R.font.oswald_semibold, FontWeight.SemiBold),
+    Font(R.font.oswald_extralight, FontWeight.ExtraLight),
+    Font(R.font.oswald_light, FontWeight.Light),
+    Font(R.font.oswald_medium, FontWeight.Medium),
+    Font(R.font.oswald_regular, FontWeight.Normal),
+)
+
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    defaultFontFamily = FontFamily.SansSerif,
+    defaultFontFamily = fontFamily,
     h1 = TextStyle(
         fontWeight = FontWeight.Bold,
         fontSize = 16.sp,
